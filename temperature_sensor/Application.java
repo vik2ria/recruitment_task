d79a29827f32bc321c0655f31a8d5190c489b6be
vik2ria <77890073+vik2ria@users.noreply.github.com>
2022-09-16 14:43:59 +0200

    rename method

--- temperature_sensor/Application.java
+++ temperature_sensor/Application.java
@@ -3,7 +3,7 @@ package temperature_sensor;
 public class Application{
 
 public static void main(String[] args) {
-    TemperatureReader.readTemperature();
+    TemperatureReader.getTemperature();
     System.out.println("hello world");
 }
 
