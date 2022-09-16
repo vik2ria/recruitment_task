4a4c0e66cd8670f2d34faf076ebe94a62df3a498
vik2ria <77890073+vik2ria@users.noreply.github.com>
2022-09-16 14:44:36 +0200

    Add converter method

--- temperature_sensor/TemperatureReader.java
+++ temperature_sensor/TemperatureReader.java
@@ -5,14 +5,38 @@ import java.util.Scanner;
 
 public class TemperatureReader {
 
-    public static void readTemperature() {
+    private static double convertADCToCelsius(double adcValue) {
+        //rougly 41.39 messured in ADC per Celsius
+        double adcPerCelsius = 41.39;
+        //baseline 0C = 2048
+        double baselineCelsius = 2048.00;
+        double temperaturIncelsius;
+
+        //Calculating the conversion and rounding to two decimals
+        temperaturIncelsius = Math.round((((adcValue - baselineCelsius) / adcPerCelsius) *100.0)) / 100.0;
+
+        return temperaturIncelsius;
+    }
+
+    private static 
+
+
+    public static void getTemperature() {
         try {
             File measuredTemperature = new File("temperature_sensor\\temperature.txt");
             Scanner readMeasuredTemperature = new Scanner(measuredTemperature);
             while(readMeasuredTemperature.hasNextLine()){
-                String stringTemperature = readMeasuredTemperature.nextLine();
-                double temperature = Double.valueOf(stringTemperature);
-                System.out.println(temperature);
+                try { 
+                String stringValueFromADC = readMeasuredTemperature.nextLine();
+                double doubleValueFromADC = Double.valueOf(stringValueFromADC);
+                
+                System.out.println(convertADCToCelsius(doubleValueFromADC));
+                Thread.sleep(100); 
+                    
+                } catch (InterruptedException e) {
+                    // TODO: handle exception
+                }
+               
             }
             readMeasuredTemperature.close();
         } catch (FileNotFoundException e) {
@@ -20,5 +44,7 @@ public class TemperatureReader {
             e.printStackTrace();
         }
     }
+
+
     
 }
