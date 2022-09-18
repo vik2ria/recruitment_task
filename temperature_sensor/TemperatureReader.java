300395f9a11f553760a45a3c052d4f9403b05a03
vik2ria <77890073+vik2ria@users.noreply.github.com>
2022-09-18 19:03:15 +0200

    start logic of time intervall and values

--- temperature_sensor/TemperatureReader.java
+++ temperature_sensor/TemperatureReader.java
@@ -1,6 +1,8 @@
 package temperature_sensor;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TemperatureReader {
@@ -18,33 +20,76 @@ public class TemperatureReader {
         return temperaturIncelsius;
     }
 
-    private static 
+    private static void temperatureLastTwoMinutes() {
+
+    }
 
 
     public static void getTemperature() {
         try {
             File measuredTemperature = new File("temperature_sensor\\temperature.txt");
             Scanner readMeasuredTemperature = new Scanner(measuredTemperature);
+            
+            LocalDateTime startTimeOfReadingOfValues = LocalDateTime.now().withNano(0);
+            // TODO: change to 2min
+            LocalDateTime endTimeOfReadingOfValues = startTimeOfReadingOfValues.plusSeconds(15);
+            
+            // set the variabels to be a value that is unlikely to get passed from the sensore            
+            double minTemperature = 5000;
+            double maxTemperature = 5000;
+            double avgTemperature = 0.0;
+            int valueCounter = 0;
+
             while(readMeasuredTemperature.hasNextLine()){
                 try { 
-                String stringValueFromADC = readMeasuredTemperature.nextLine();
-                double doubleValueFromADC = Double.valueOf(stringValueFromADC);
-                
-                System.out.println(convertADCToCelsius(doubleValueFromADC));
-                Thread.sleep(100); 
                     
-                } catch (InterruptedException e) {
+                    String stringValueFromADC = readMeasuredTemperature.nextLine();
+                    double doubleValueFromADC = Double.valueOf(stringValueFromADC);
+                    double doubleValueInCelsius = convertADCToCelsius(doubleValueFromADC);
+
+                    valueCounter ++;
+                    
+                    avgTemperature += doubleValueInCelsius;
+                    
+                    LocalDateTime currentDateTime = LocalDateTime.now().withNano(0);
+
+                    if(endTimeOfReadingOfValues.equals(currentDateTime)) {
+                        avgTemperature = avgTemperature / valueCounter;
+                        System.out.println("min: " + minTemperature + "max: " + maxTemperature + "avg: " + (avgTemperature/valueCounter));
+                        startTimeOfReadingOfValues = LocalDateTime.now().withNano(0);
+                        // TODO: change to min
+                        endTimeOfReadingOfValues = startTimeOfReadingOfValues.plusSeconds(15);
+                    }
+
+                    if(minTemperature == 5000 && maxTemperature == 5000) {
+                    minTemperature = doubleValueInCelsius;
+                    maxTemperature = doubleValueInCelsius;
+                    }
+
+                    if(minTemperature > doubleValueInCelsius){
+                        minTemperature = doubleValueInCelsius;
+                    }
+
+                    if(maxTemperature < doubleValueInCelsius) {
+                        maxTemperature = doubleValueInCelsius;
+                    }
+                    
+                   
+                    Thread.sleep(100); 
+                    
+                 } catch (InterruptedException e) {
                     // TODO: handle exception
-                }
+                    }
                
-            }
+                }
             readMeasuredTemperature.close();
-        } catch (FileNotFoundException e) {
+            } catch (FileNotFoundException e) {
             System.out.println("Ingen temperatur funnet");
             e.printStackTrace();
-        }
+            }
     }
 
-
-    
 }
+    
+
+
