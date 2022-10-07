bf6b95a5b44092e6e1333d2df017c131ee2e968b
vik2ria <77890073+vik2ria@users.noreply.github.com>
2022-10-07 15:40:31 +0200

    add maven

---
+++ src/main/java/temperature_sensor/Application.java
@@ -0,0 +1,11 @@
+package temperature_sensor;
+
+public class Application{
+
+    public static void main(String[] args) {
+        TemperatureReader.getTemperature();
+        System.out.println("hello world");
+    }
+
+}
+
---
+++ src/main/java/temperature_sensor/TemperatureReader.java
@@ -0,0 +1,166 @@
+package temperature_sensor;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import org.json.*;
+
+public class TemperatureReader {
+
+    private static double roundingDouble(Double doubleValueToRound) {
+        double returnValue = Math.round(doubleValueToRound) /100.0;
+        return returnValue;
+    }
+
+    private static double convertADCToCelsius(double adcValue) {
+        //rougly 41.39 messured in ADC per Celsius
+        double adcPerCelsius = 41.39;
+        //baseline 0C = 2048
+        double baselineCelsius = 2048.00;
+        double temperaturIncelsius;
+
+        //Calculating the conversion and rounding to two decimals
+        temperaturIncelsius = roundingDouble(((adcValue - baselineCelsius) / adcPerCelsius));
+
+        return temperaturIncelsius;
+    }
+
+    private static void postJSONToApi(JSONObject jsonTempObject) {
+        try {
+            URL successfulTempReading = new URL("http://localhost:5000/api/temperature");
+            HttpURLConnection apiCon = (HttpURLConnection)successfulTempReading.openConnection();
+
+            apiCon.setRequestMethod("POST");
+            apiCon.setRequestProperty("Content-type", "application/json");
+            apiCon.setRequestProperty("Accept", "application/json");
+            apiCon.setDoOutput(true);
+
+            try(OutputStream outputStream = apiCon.getOutputStream()) {
+                String jsonStringWithTemp = jsonTempObject.toString();
+                byte[] input = jsonStringWithTemp.getBytes("utf-8");
+                outputStream.write(input, 0, input.length);
+            }
+
+            try(BufferedReader br = new BufferedReader(new InputStreamReader(apiCon.getInputStream(), "utf-8"))){
+                StringBuilder response = new StringBuilder();
+                String responseLine = null;
+                while((responseLine = br.readLine()) != null) {
+                    response.append(responseLine.trim());
+                }
+                System.out.println(response.toString());
+
+            }
+
+
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void getTemperature() {
+        try {
+            File measuredTemperature = new File("src/main/java/temperature_sensor/temperature.txt");
+            Scanner readMeasuredTemperature = new Scanner(measuredTemperature);
+
+            LocalDateTime startTimeOfReadingOfValues = LocalDateTime.now().withNano(0);
+            // TODO: change to 2min
+            LocalDateTime endTimeOfReadingOfValues = startTimeOfReadingOfValues.plusSeconds(30);
+
+            // set the variables to be a value that is unlikely to get passed from the sensore
+            double minTemperature = 5000;
+            double maxTemperature = 5000;
+            double avgTemperature = 0.0;
+            int valueCounter = 0;
+
+            //Hashmap to contain the readings
+
+
+            while(readMeasuredTemperature.hasNextLine()){
+                try {
+
+                    String stringValueFromADC = readMeasuredTemperature.nextLine();
+                    double doubleValueFromADC = Double.valueOf(stringValueFromADC);
+                    double doubleValueInCelsius = convertADCToCelsius(doubleValueFromADC);
+
+                    valueCounter ++;
+
+                    avgTemperature += doubleValueInCelsius;
+
+                    LocalDateTime currentDateTime = LocalDateTime.now().withNano(0);
+
+                    if(endTimeOfReadingOfValues.equals(currentDateTime)) {
+                        // calculate avg temperature
+                        avgTemperature = avgTemperature / valueCounter;
+
+                        //hashmap to give to JSONObject
+                        HashMap<String, LocalDateTime> startAndEnTime = new HashMap<>();
+                        startAndEnTime.put("start", startTimeOfReadingOfValues);
+                        startAndEnTime.put("end", endTimeOfReadingOfValues);
+                        //JSONObject
+                        JSONObject jsonTemperatureReading = new JSONObject();
+                        jsonTemperatureReading.put("time", startAndEnTime);
+                        jsonTemperatureReading.put("min", minTemperature);
+                        jsonTemperatureReading.put("max", maxTemperature);
+                        jsonTemperatureReading.put("avg", avgTemperature);
+
+                        postJSONToApi(jsonTemperatureReading);
+
+                        System.out.println("min: " + minTemperature + "max: " + maxTemperature + "avg: " + roundingDouble(avgTemperature));
+
+                        //Reset variables
+                        minTemperature = 5000;
+                        maxTemperature = 5000;
+                        avgTemperature = 0.0;
+                        valueCounter = 0;
+
+                        //set start and end time for next interval
+                        startTimeOfReadingOfValues = LocalDateTime.now().withNano(0);
+                        // TODO: change to min
+                        endTimeOfReadingOfValues = startTimeOfReadingOfValues.plusSeconds(30);
+                    }
+
+                    if(minTemperature == 5000 && maxTemperature == 5000) {
+                        minTemperature = doubleValueInCelsius;
+                        maxTemperature = doubleValueInCelsius;
+                    }
+
+                    if(minTemperature > doubleValueInCelsius){
+                        minTemperature = doubleValueInCelsius;
+                    }
+
+                    if(maxTemperature < doubleValueInCelsius) {
+                        maxTemperature = doubleValueInCelsius;
+                    }
+
+
+                    Thread.sleep(100);
+
+                } catch (InterruptedException | JSONException e) {
+                    // TODO: handle exception
+                }
+
+            }
+            readMeasuredTemperature.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Ingen temperatur funnet");
+            e.printStackTrace();
+        }
+    }
+
+}
+
+
+
